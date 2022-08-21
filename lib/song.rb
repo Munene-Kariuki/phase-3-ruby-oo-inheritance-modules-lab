c1ff80a3b4a055268bfa899772a10fe81d27bc66
Denis <denismunene999@gmail.com>
2022-08-21 12:50:51 +0300

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,6 @@ class Song
 
   @@songs = []
 
-  def initialize
-    super
-  end
-
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
@@ -25,19 +21,8 @@ class Song
     @@songs
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
-
   def artist=(artist)
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
